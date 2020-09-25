f600223d2fffb5296c038b1359a5c7be2d0f7bc6
Tatsiana Novik <Tatsiana_Novik@epam.com>
2020-09-25 11:15:59 +0300

    feat: add task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+    let result = '';
+    if (arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === 'string') {
+                result = result + (arr[i].trim())[0];
+            }
+        }
+    }
+    result = result.toUpperCase();
+    result = result.split('');
+    result = result.sort();
+    result = result.join('');
+    return result;
 };
