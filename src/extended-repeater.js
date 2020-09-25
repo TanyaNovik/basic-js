47abb5aec27648869af5441ea11ed0517e74d6d5
Tatsiana Novik <Tatsiana_Novik@epam.com>
2020-09-25 23:19:37 +0300

    feat: add task extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    options.separator = options.separator ? options.separator : '+';
+    options.additionSeparator = options.additionSeparator ? options.additionSeparator : '|';
+    if (typeof options.addition === 'undefined') {
+        options.addition = ''
+    } else if (options.addition === null) {
+        options.addition = 'null';
+    } else {
+        options.addition =  options.addition.toString();
+    }
+    let additionSection = options.addition.toString() !== '' ? (options.additionSeparator + options.addition) : '';
+    let newLine = str + (options.addition + additionSection.repeat(options.additionRepeatTimes - 1) + options.separator + str).repeat(options.repeatTimes - 1) + options.addition + additionSection.repeat(options.additionRepeatTimes - 1);
+    return newLine;
 };
   
