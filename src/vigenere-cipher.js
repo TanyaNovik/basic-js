7626c248117e0a20f112de72e3fc980f847dc8c2
Tatsiana Novik <Tatsiana_Novik@epam.com>
2020-09-29 14:56:20 +0300

    feat: add task vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(direction) {
+        this.direction = direction;
+    }
+
+    encrypt(message, key) {
+        if (!message || !key) {
+            throw new Error('message or key is not defined')
+        }
+        message = message.toUpperCase();
+        key = key.toLowerCase();
+        let result = '';
+        for (let i = 0, j = 0; i < message.length; i++, j++) {
+            j = j === key.length ? j = 0 : j;
+            if (message[i].charCodeAt() >= 'A'.charCodeAt() && message[i].charCodeAt() <= 'Z'.charCodeAt()) {
+                let shift = key[j].charCodeAt() - 'a'.charCodeAt();
+                if ((message[i].charCodeAt() + shift) > 'Z'.charCodeAt()) {
+                    shift = 'A'.charCodeAt() + (message[i].charCodeAt() + shift - 'Z'.charCodeAt()) - 1;
+                } else {
+                    shift = message[i].charCodeAt() + shift
+                }
+                result = (this.direction === false) ? String.fromCharCode(shift) + result : result + String.fromCharCode(shift);
+
+            } else {
+                result = (this.direction === false) ? message[i] + result : result + message[i];
+                j--;
+            }
+        }
+        return result;
+    }
+
+    decrypt(message, key) {
+        if (!message || !key) {
+            throw new Error('message or key is not defined')
+        }
+        message = message.toUpperCase();
+        key = key.toLowerCase();
+        let result = '';
+        for (let i = 0, j = 0; i < message.length; i++, j++) {
+            j = j === key.length ? j = 0 : j;
+            if (message[i].charCodeAt() >= 'A'.charCodeAt() && message[i].charCodeAt() <= 'Z'.charCodeAt()) {
+                let shift = key[j].charCodeAt() - 'a'.charCodeAt();
+                if ((message[i].charCodeAt() - shift) < 'A'.charCodeAt()) {
+                    shift = 'Z'.charCodeAt() + (message[i].charCodeAt() - shift - 'A'.charCodeAt()) + 1;
+                } else {
+                    shift = message[i].charCodeAt() - shift;
+                }
+                result = (this.direction === false) ? String.fromCharCode(shift) + result : result + String.fromCharCode(shift);
+            } else {
+                result = (this.direction === false) ? message[i] + result : result + message[i];
+                j--;
+            }
+        }
+        return result;
+    }
 }
 
 module.exports = VigenereCipheringMachine;
